672f4db08577b0c9e16dfdca7e4401bb129c64d7
Devill Don <53294521+devillD@users.noreply.github.com>
2021-02-20 13:32:31 +0600

    Not working at this time

--- torlock.py
+++ torlock.py
@@ -39,7 +39,7 @@ If any error occurs, feel free to pm me on https://t.me/unkusr""", parse_mode=Pa
 def find(update: Update, context: CallbackContext) -> None:
     try:
         update.message.reply_text("Searching results for 👉{}👈".format(update.message.text))
-        #YTS api
+        #Torlock api
         url = "https://xtorrex.herokuapp.com/api/torlock/{}".format(update.message.text)
         results = requests.get(url).json()
         print(results)
