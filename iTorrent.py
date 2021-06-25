0e0fc22a7c30ae6a23f01dab2e38c009471fd5b8
Devill Don <53294521+devillD@users.noreply.github.com>
2021-06-25 14:40:17 +0600

    iTorrent api

---
+++ iTorrent.py
@@ -0,0 +1,148 @@
+import os
+import aiohttp
+import json
+from pyrogram import Client, filters, emoji
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+app = Client("trntsrcbot", api_id=int(os.environ.get("API_ID")), api_hash=os.environ.get("API_HASH"), bot_token=os.environ.get("BOT_TOKEN"))
+
+
+print("\nBot Started\n")
+
+
+@app.on_message(filters.command(['start']))
+async def start(_, message):
+    await message.reply_text("Hello I'm iTorrent Torrent Scraper Bot\nSend /help To Show Help Screen\nBot by @unkusr")
+
+
+
+@app.on_message(filters.command(['help']))
+async def help(_, message):
+    await message.reply_text("Example: /find titanic")
+
+m = None
+i = 0
+a = None
+query = None
+
+
+@app.on_message(filters.command(["find"]))
+async def find(_, message):
+    global m
+    global i
+    global a
+    global query
+    try:
+        await message.delete()
+    except:
+        pass
+    if len(message.command) < 2:
+        await message.reply_text("Usage: /find query")
+        return
+    query = message.text.split(None, 1)[1].replace(" ", "%20")
+    m = await message.reply_text("Searching")
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://campestral-rugs.000webhostapp.com/?name={query}") \
+                    as resp:
+                a = json.loads(await resp.text())
+    except:
+        await m.edit("Found Nothing.")
+        return
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"➲Name: {a[i]['name']}\n"
+        f"➲Link: `{a[i]['link']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"Next",
+                                         callback_data="next"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="delete")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@app.on_callback_query(filters.regex("next"))
+async def callback_query_next(_, message):
+    global i
+    global m
+    global a
+    global query
+    i += 1
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"➲Name: {a[i]['name']}\n"
+        f"➲Link: `{a[i]['link']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"Prev",
+                                         callback_data="previous"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="delete"),
+                    InlineKeyboardButton(f"Next",
+                                         callback_data="next")
+                    
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@app.on_callback_query(filters.regex("previous"))
+async def callback_query_previous(_, message):
+    global i
+    global m
+    global a
+    global query
+    i -= 1
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"➲Name: {a[i]['name']}\n"
+        f"➲Link: `{a[i]['link']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"Prev",
+                                         callback_data="previous"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="delete"),
+                    InlineKeyboardButton(f"Next",
+                                         callback_data="next")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@app.on_callback_query(filters.regex("delete"))
+async def callback_query_delete(_, message):
+    global m
+    global i
+    global a
+    global query
+    await m.delete()
+    m = None
+    i = 0
+    a = None
+    query = None
+
+
+app.run()
