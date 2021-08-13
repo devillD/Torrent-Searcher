5fffa32a8838466c7d1b7e7c6ef9ad8c4491c6eb
Devill Don <53294521+devillD@users.noreply.github.com>
2021-08-13 20:36:10 +0600

    Update piratebay.py

--- piratebay.py
+++ piratebay.py
@@ -44,7 +44,7 @@ async def find(_, message):
     m = await message.reply_text("Searching")
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api-tor.herokuapp.com/piratebay/{query}") \
+            async with session.get(f"https://api-abirxox.cloud.okteto.net/piratebay/{query}") \
                     as resp:
                 a = json.loads(await resp.text())
     except:
