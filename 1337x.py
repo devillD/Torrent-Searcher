4c670cba4853293fe8452114a93dd461d9af7d2a
Devill Don <53294521+devillD@users.noreply.github.com>
2021-08-16 15:42:21 +0600

    Falling back to old api

--- 1337x.py
+++ 1337x.py
@@ -44,7 +44,7 @@ async def find(_, message):
     m = await message.reply_text("Searching")
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api-tor.herokuapp.com/1337x/{query}") \
+            async with session.get(f"https://api.api-zero.workers.dev/1337x/{query}") \
                     as resp:
                 a = json.loads(await resp.text())
     except:
